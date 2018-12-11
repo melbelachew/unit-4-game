a2a68f4de9ffbbc99f31d8e5153c2bd9bf36a584
Melat Belachew <melbelachew@gmail.com>
2018-12-11 01:50:53 -0500

    updated

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,58 +3,78 @@
 var randomNumber;
 var wins=0;
 var losses=0;
-var firstNumber = 0;
+var score = 0;
+var imgs=["./assets/images/Img1.png", "./assets/images/Img2.png","./assets/images/Img3.png","./assets/images/Img5.png",]
 
 //make 4 divs for crystals and assign a class then generate random numer for each crystal
 
+//wrap the forloop iteration into a reset function
+
 for(var i =0; i<4; i++) {
     
-    var randomNumber = Math.floor(Math.random()*(12-1)+1);
+    var random = Math.floor(Math.random()*(12-1)+1);
 
-    // console.log(randomNumber);
+    // console.log(random);
 
-    var crystal = $("<div>");
+    var crystal = $("<img>");
 
-        crystal.attr({"class": 'crystal', "value-random":randomNumber});
+        crystal.attr({"class": 'crystals', "value-random":random});
+        crystal.attr("src",imgs[i]);
+        crystal.attr("height","100");
 
        $(".crystals").append(crystal);
+    console.log(crystal)
+}   
 
-}
+//Reset function
+
+function resetCrystals () {
+    for(var i =0; i<4; i++) {
+        var crystal = $("<img>");
+
+        crystal.attr({"class": 'crystals', "value-random":random});
+        crystal.attr("src",imgs[i]);
+        crystal.attr("height","100");
+
+       $(".crystals").append(crystal);
+}}
+  
 
 //random selection
 
- resultNumber = Math.floor(Math.random() * (120 - 19) + 19);
+resultNumber = Math.floor(Math.random() * (120 - 19) + 19);
 
 //  console.log(resultNumber);
 
- $("#result").html('Result: ' + resultNumber);
+$("#randomNumGenerated").append(resultNumber);
 
   
  //onclick function on crystals
 
- $(".crystal").on('click', function() {
+ $(".crystals").on('click', function() {
 
-    // console.log($(this));
+    console.log($(this));
 
     var num = parseInt($(this).attr('value-random'));
 
-    firstNumber += num;
+    score += num;
 
-    // console.log(firstNumber);
+    $("#totalScore").text(score);
+
     
     //conditional statement for wins and losses
 
 
-    if(firstNumber > randomNumber){
+    if(score > randomNumGenerated){
 
-        // console.log("lost")
+        // console.log("losses")
         losses--;
         
         $("#losses").html('You lost');
     }
 
-    else if (firstNumber === randomNumber){
-        // console.log("win");
+    else if (score === randomNumGenerated){
+        // console.log("wins");
         wins++;
 
         $("#wins").html('You win');
